bd54fa8bcb9be3f12346f1895d4da8fd3dc02d5b
T19 <2462370039@qq.com>
2022-08-11 06:23:31 +0800

    第一次上传

---
+++ app/src/main/java/com/team8/demo_app_gradle/MainActivity.java
@@ -0,0 +1,14 @@
+package com.team8.demo_app_gradle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
