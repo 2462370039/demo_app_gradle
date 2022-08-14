fe512367a85108d90d685e9f1449d7988ce659f7
T19 <2462370039@qq.com>
2022-08-14 20:38:23 +0800

    第一次测试了Navigation

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # demo_app_gradle
+[Android开发学习(JetPack)](https://www.bilibili.com/video/BV1w4411t7UQ)
--- app/src/main/java/com/team8/demo_app_gradle/ui/MainUiActivity.java
+++ app/src/main/java/com/team8/demo_app_gradle/ui/MainUiActivity.java
@@ -1,6 +1,10 @@
 package com.team8.demo_app_gradle.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
@@ -12,5 +16,20 @@ public class MainUiActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_ui);
+
+        //设置顶部栏
+        /* 报错：does not have a NavController set on
+        NavController controller = Navigation.findNavController(this, R.id.fragmentContainerView);
+        NavigationUI.setupActionBarWithNavController(this, controller);*/
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView);
+        NavController controller = navHostFragment.getNavController();
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController controller = Navigation.findNavController(this, R.id.fragmentContainerView);
+        return controller.navigateUp();
+        //NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView);
+        //NavController controller = navHostFragment.getNavController();
     }
 }
