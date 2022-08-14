132ee88ec435b1ec456e2108744304325554f269
T19 <2462370039@qq.com>
2022-08-14 14:38:26 +0800

    第二次测试了LiveData

--- app/src/main/java/com/team8/demo_app_gradle/ui/MainUiActivity.java
+++ app/src/main/java/com/team8/demo_app_gradle/ui/MainUiActivity.java
@@ -1,4 +1,4 @@
-package com.team8.demo_app_gradle.list;
+package com.team8.demo_app_gradle.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import com.team8.demo_app_gradle.R;
 
-public class List_options_activity extends AppCompatActivity {
+public class MainUiActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list_options);
+        setContentView(R.layout.activity_main_ui);
     }
 }
